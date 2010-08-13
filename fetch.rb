9cd596656fcaa19649cb518bbf7ee3898f29988e
Rafał Lisowski <lisukorin@gmail.com>
2010-08-13 21:51:30 +0200

    ignore . directory

--- fetch.rb
+++ fetch.rb
@@ -1,6 +1,6 @@
 # go into each directory and run fetch script
 dir = Dir.new(File.dirname(__FILE__))
 dir.each do |subdir|
-	next unless File.directory?(subdir) && subdir !~ /\..+/
+	next unless File.directory?(subdir) && subdir !~ /\..?/
 	`ruby #{File.join(File.dirname(__FILE__), subdir, 'fetch.rb')}`
 end
